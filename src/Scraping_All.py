2e79a0228bc4470d1be3b5b15bc57fc1324e4332
jgonzalezleal <jgonzalezleal@gmail.com>
2020-04-12 10:47:41 +0200

    Modificado el generador del vector de fechas con el control de fecha_final>=fecha_inicio. Eliminada programación en main y pasada a funciones. Ahora todo el dataset se guarda en memoria hasta el final del proceso en donde se vuelca a disco. Se han incluido dos puntos de control en disco por si falla una de las descargas, no repetir todo el proceso (tarda 1h para 3 años)

--- src/Scraping_All.py
+++ src/Scraping_All.py
@@ -2,30 +2,25 @@ from tools import *
 from Scraping_MercaAlicante import *
 from Scraping_MercaMadrid import *
 import datetime as dt
+from time import time
 
 def date_vector(año_inicio, año_fin, mes_inicio, mes_final, dia):
     # It generates a date vector with dates.
     # Output: a vector of strings 
     fecha=[]
-    for año in range(año_inicio, año_fin +1):
-        for mes in range(mes_inicio,mes_final +1):
-            fecha.append(str(dt.date(año, mes, dia)))
+    if año_inicio<= año_fin:
+        if mes_inicio <= mes_final:
+            for año in range(año_inicio, año_fin +1):
+                for mes in range(mes_inicio,mes_final +1):
+                    fecha.append(str(dt.date(año, mes, dia)))
     return(fecha)
-    
 
-# Main program    
-if '__main__' == __name__:
-    # Date Extraction from MercAlicante
-    
-    fecha_inicio = date_vector(2017, 2019, 1, 12, 1)
-        
-    if 1==0:
-        print('MercAlicante Data Extractor')
-        print('--------------------------')
-        producto='' #Void all products
-        familia='' # Void all families
-        categoria = '106' # Frutas 
-        pos=1
+def MercAlicante_scraper(producto, familia, categoria, fecha_inicio):
+    print('MercAlicante Data Extractor')
+    print('--------------------------')
+    pos=1
+    # Call to website by month
+    try:
         for fecha in fecha_inicio:
             # Calculate the last day of the month or current day
             fecha_Fin = str(last_day_Month(fecha))
@@ -34,62 +29,71 @@ if '__main__' == __name__:
             df_Mercalicante=df if pos==1 else pd.concat([df_Mercalicante, df])
             pos +=1
             
-        Save_df_to_csv(df_Mercalicante,'MercAlicante',fecha_inicio[0], str(last_day_Month(fecha_inicio[-1])))
-    if 1==0:    
-        # Date Extraction from MercaMadrid
-        print('')
-        print('MercaMadrid Data Extractor')
-        print('--------------------------')
-        browser = 'Chrome'
-        pos=1
-        for fecha in fecha_inicio:
+        #file_name= Save_df_to_csv(df_Mercalicante,'MercAlicante',fecha_inicio[0], str(last_day_Month(fecha_inicio[-1])))
+        #return(file_name)
+        return(df_Mercalicante)
+    except:
+        print('An error existing during MercAlicante extraction.')
+        return(None)
 
+def MercaMadrid_scraper(browser, mercado, fecha_inicio):
+    # Date Extraction from MercaMadrid
+    print('')
+    print('MercaMadrid Data Extractor')
+    print('--------------------------')
+    pos=1
+    try:
+        # Call to website by month
+        for fecha in fecha_inicio:
             fecha_fin = str(last_day_Month(fecha))
-            df= Scraping_MercaMardrid(browser,'MERCADO CENTRAL DE FRUTAS', fecha, fecha_fin)
+            df= Scraping_MercaMardrid(browser, mercado, fecha, fecha_fin)
             df_MercaMadrid = df if pos==1 else pd.concat([df_MercaMadrid, df])
             pos +=1
-        Save_df_to_csv(df_MercaMadrid,'MercaMadrid',fecha_inicio[0], str(last_day_Month(fecha_inicio[-1])))
-    
-    # Unificando datasets
-    #columns = ['Año', 'Mes', 'Origen','Producto', 'Precio']
-    
-    if 1==0:
-        # MercAlicante
-        df1= pd.read_csv('MercAlicante_20190101_20191231.csv', sep=';')
-        df2= pd.read_csv('MercAlicante_20180101_20181231.csv', sep=';')
-        df3= pd.read_csv('MercAlicante_20170101_20171231.csv', sep=';')
-        print('Datasets MercaMadrid loaded')
-        print('Size: ', len(df1), len(df2), len(df3))
-        df1 = pd.concat([df1.reset_index(), df2.reset_index()], ignore_index=True)
-        df1 = pd.concat([df1.reset_index(), df3.reset_index()], ignore_index=True)
-        columns = ['Producto','Frecuente','Año','Mes','Origen']
-        df1 = df1[columns] # Select the columns
-        df1.rename(columns={'Frecuente' : 'Precio'}, inplace= True) 
-        Save_df_to_csv(df1,'MercAlicante','2017-01-01', '2019-12-31')
-        print('Total rows: ', len(df1))
-    if 1==0:
-        # MercaMadrid
-        df1= pd.read_csv('MercaMadrid_20190101_20191231.csv', sep=';')
-        df2= pd.read_csv('MercaMadrid_20180101_20181231.csv', sep=';')
-        df3= pd.read_csv('MercaMadrid_20170101_20171231.csv', sep=';')
-        print('Datasets MercaMadrid loaded')
-        print( 'Size: ', len(df1), len(df2), len(df3))
-        df1 = pd.concat([df1.reset_index(), df2.reset_index()], ignore_index=True)
-        df1 = pd.concat([df1.reset_index(), df3.reset_index()], ignore_index=True)
-        # Algunos registros no tienen informado el campo Producto, pero sí el campo Variedad
-        
-        columns = ['Variedad','Precio frecuente','Año','Mes','Origen']
-        df1 = df1[columns]
-        df1.rename(columns={'Precio frecuente' : 'Precio', 'Variedad':'Producto'}, inplace= True)
-        Save_df_to_csv(df1,'MercaMadrid','2017-01-01', '2019-12-31')
-        print('Total rows: ', len(df1))
+        #file_name=Save_df_to_csv(df_MercaMadrid,'MercaMadrid',fecha_inicio[0], str(last_day_Month(fecha_inicio[-1])))
+        #return(file_name)
+        return(df_MercaMadrid)
+    except:
 
-    if 1==0:
-        # Unificación MercAlicante y MercaMadrid
-        df1= pd.read_csv('MercAlicante_20170101_20191231.csv', sep=';')
-        df2= pd.read_csv('MercaMadrid_20170101_20191231.csv', sep=';')
-        print('MercAlicante and MercaMadrid Union ')
-        print( 'Size: ', len(df1), len(df2))
-        df1 = pd.concat([df1.reset_index(), df2.reset_index()], ignore_index=True)
-        print('Total rows: ', len(df1))
-        Save_df_to_csv(df1,'Mercados Centrales','2017-01-01', '2019-12-31')
+        return(None)
+
+# Main program    
+if '__main__' == __name__:
+    # Generation of date vector
+    # date_vector(fecha inicio, fecha fin, mes inicio, mes fin, dia)
+    # Ex: ['2017-01-01', '2017-02-01', '2017-03-01'.... '2019-11-01', '2019-12-01']
+    start_time = time()
+    fecha_inicio = date_vector(2017, 2019, 1, 12, 1)
+    #file_name_vector=[]
+    columns = ['Producto','Precio','Año','Mes','Origen']
+    df = pd.DataFrame(columns=columns)    
+    if 1==1:
+        # Date Extraction from MercAlicante
+        # Options:
+        producto='' #Void all products
+        familia='' # Void all families
+        categoria = '106' # An option of the website: Frutas 
+
+        resp = MercAlicante_scraper(producto, familia, categoria, fecha_inicio)
+        # Control Point 
+        Save_df_to_csv(resp,'Mercalicante', fecha_inicio[0], str(last_day_Month(fecha_inicio[-1])))
+        #file_name_vector.append(resp)
+        df = pd.concat([df, resp], ignore_index=True)
+        print('Total rows of MercAlicante:', len(resp))
+        print('Execution time (min): ', round((time() - start_time)/60, 2))
+    if 1==1:
+        # Date Extraction from MercaMadrid
+        # Options:    
+        browser = 'Chrome' # browser name of Selenium
+        mercado = 'MERCADO CENTRAL DE FRUTAS'  # An option of the website. 
+
+        resp = MercaMadrid_scraper(browser, mercado, fecha_inicio) 
+        # Control Point
+        Save_df_to_csv(resp,'MercaMadrid', fecha_inicio[0], str(last_day_Month(fecha_inicio[-1])))
+        print('Total rows of MercaMadrid:', len(resp))
+        #file_name_vector.append(resp)
+        df = pd.concat([df, resp], ignore_index=True)
+        print('Execution time (min): ', round((time() - start_time)/60, 2))
+    # Unificando datasets
+    if 1==1:
+        print('Total rows: ', len(df))
+        Save_df_to_csv(df,'Mercados Centrales', fecha_inicio[0], str(last_day_Month(fecha_inicio[-1])))
